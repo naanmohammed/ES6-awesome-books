ee947c6fce13f7aee90ff48e1d2740db25d3e03b
Naan Mohammed <naanahmed24@gmail.com>
2023-04-05 01:09:20 +0300

    Add index.js

---
+++ modules/index.js
@@ -0,0 +1,16 @@
+import { BookList } from './bookList.js';
+import { hideSectionsOnClick } from './navigation.js';
+import { displayDate } from './date.js';
+
+
+const bookListElement = document.getElementById('book-list');
+const addBookFormElement = document.getElementById('add-book-form');
+const titleInputElement = document.getElementById('title-input');
+const authorInput = document.getElementById('author-input');
+
+const bookList = new BookList(bookListElement, addBookFormElement, titleInputElement, authorInput);
+bookList.loadBooks();
+
+hideSectionsOnClick();
+
+window.addEventListener('load', displayDate);
