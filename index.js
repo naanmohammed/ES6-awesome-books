46545c191b17a11cfe4c90bd7e4d03caf73d31eb
Naan Mohammed <naanahmed24@gmail.com>
2023-04-05 12:23:43 +0300

    Update date.js to display date correctly

--- index.js
+++ index.js
@@ -10,6 +10,4 @@ const authorInput = document.getElementById('author-input');
 const bookList = new BookList(bookListElement, addBookFormElement, titleInputElement, authorInput);
 bookList.loadBooks();
 
-hideSectionsOnClick();
-
 window.addEventListener('load', displayDate);
--- modules/date.js
+++ modules/date.js
@@ -2,5 +2,6 @@ import { DateTime } from '../node_modules/luxon/src/luxon.js';
 
 export default function displayDate() {
   const now = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+  console.log(now);
   document.getElementById('date-time').textContent = now;
 }
